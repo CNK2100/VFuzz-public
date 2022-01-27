5215939cf32a071c4ce85b75e18689a7ab78d6b3
CNK2100 <58585946+CNK2100@users.noreply.github.com>
2022-01-27 09:36:32 +0900

    Update fuzzer_config.py

--- src/fuzzer_config.py
+++ src/fuzzer_config.py
@@ -3,13 +3,13 @@
 This is the configuration file for VFuzz.
 """
 
-# Z-Wave home ID to fuzz in Hexadecimal in type String
+# Z-Wave home ID to fuzz in Hexadecimal
 homeid =0xF8A86B6B
 
-# Z-Wave Target device node ID in hex
-nodeid =0x02 ###"02" #"4E" #  0x4E
+# Z-Wave Target device node ID in Hexadecimal
+nodeid =0x02 
 
-# Devices Z-Wave Frequency. uncomment your devices frequency and comment remaining
+# Z-Wave Frequency used. Select your devices frequency and comment remaining
 zwaveFrequency = 908420000  ### FOR USA
 # zwaveFrequency = 868399841  ### For EU
 
@@ -20,9 +20,4 @@ verbose_vfuzz= False  # or True
 timeoutFuzzer = 86400  ## 86400 seconds = 24 hours
 #
 # # field mutation timeout
-timeout_field_Mut = 18000  ## 3600 in Seconds
-#
-
-
-
-
+timeout_field_Mut = 18000  ## in seconds
